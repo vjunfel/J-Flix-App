74f9702aa08cd882560e4fb27ed77c9799eed867
vjunfel <vjunfel@gmail.com>
2023-05-20 00:09:39 +0800

    added slider image using swiper JS on movie page.

--- js/script.js
+++ js/script.js
@@ -12,6 +12,55 @@ function hightlightActiveLink() {
 	});
 }
 
+// Display Now Playing Movie Slider
+async function displaySlider() {
+	const { results } = await fetchAPIData('movie/now_playing');
+
+	results.forEach((movie) => {
+		const div = document.createElement('div');
+		div.classList.add('swiper-slide');
+
+		div.innerHTML = `
+			<a href='movie-details.html?id=${movie.id}'>
+				<img src='https://image.tmdb.org/t/p/w400${movie.poster_path}' alt='${movie.title}'/>
+			</a>
+			<h4 class='swiper-rating'>
+				<i class='fas fa-star text-secondary'></i> ${movie.vote_average} / 10
+			</h4>
+			`;
+		document.querySelector('.swiper-wrapper').appendChild(div);
+	});
+
+	initSwiper();
+}
+
+function initSwiper() {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		freeMode: true,
+		loop: true,
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 2,
+			},
+			700: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+			1500: {
+				slidesPerView: 5,
+			},
+		},
+	});
+}
+
 // Displaying 20 popoular movies from TMDB_API endpoint
 async function displayPopularMovies() {
 	const { results } = await fetchAPIData('movie/popular');
@@ -148,8 +197,6 @@ async function displayShowDetails() {
 
 	const show = await fetchAPIData(`tv/${showId}`);
 
-	console.log(show);
-
 	// Overlay for background image
 	displayBackgroundImage('tv', show.backdrop_path);
 
@@ -269,6 +316,7 @@ function init() {
 	switch (global.currentPage) {
 		case '/':
 		case '/index.html':
+			displaySlider();
 			displayPopularMovies();
 			break;
 		case '/shows.html':
