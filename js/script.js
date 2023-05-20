72919b0f8adf00fd972adffe8fc32c6945827452
vjunfel <vjunfel@gmail.com>
2023-05-21 04:01:51 +0800

    display the search results into the search page

--- js/script.js
+++ js/script.js
@@ -33,10 +33,18 @@ async function search() {
 	global.search.term = urlParams.get('search-term');
 
 	if (global.search.term !== '' && global.search.term !== null) {
-		const results = await searchAPIData();
-		console.log(results);
+		const {results, total_pages, page} = await searchAPIData();
+
+		if (results.length === 0) {
+			showAlert('No Results Found!', 'alert-error');
+			return;
+		} 
+
+		displaySearchResults(results);
+
+		showAlert('Success!', 'alert-success');
 	} else {
-		showAlert('* Please Enter Search Keyword');
+		showAlert('* Please Enter Search Keyword', 'alert-error');
 	}
 }
 
@@ -54,6 +62,34 @@ async function searchAPIData() {
 	const data = await response.json();
 	hideSpinner();
 	return data;
+}
+
+// Display Search Results
+function displaySearchResults(results) {
+	// const { results } = await fetchAPIData('result/popular');
+
+	results.forEach((result) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+            <a href="${global.search.type}-details.html?id=${result.id}">
+            ${ result.poster_path 
+				? `<img src="https://image.tmdb.org/t/p/w500${ result.poster_path }" 
+					class="card-img-top"
+					alt="${global.search.type === 'movie' ? result.title : result.name}" />`
+				: `<img src="../images/no-image.jpg" class="card-img-top"
+					alt="${global.search.type === 'movie' ? result.title : result.name}"/>`
+			}
+            </a>
+            <div class="card-body">
+				<h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+				<p class="card-text">
+					<small class="text-muted">Release: ${ global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+				</p>
+        	</div>`;
+
+		document.querySelector('#search-results').appendChild(div);
+	});
 }
 
 // Show Alert
